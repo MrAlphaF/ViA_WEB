5a549a95b7187c809afb4a408281162235a7027b
coomzilla <THIHyb@gmail.com>
2025-05-27 15:19:34 +0300

    Fixed bad CSS in add servics page

--- assignment1_project_name/admin/add_service.php
+++ assignment1_project_name/admin/add_service.php
@@ -100,7 +100,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin - Add New Service</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
